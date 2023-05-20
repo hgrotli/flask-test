c4c0260e6a576769c6c59bd75921a91cfcfbfab3
Vegard Madsen <vegarsma@stud.ntnu.no>
2023-05-21 00:57:41 +0200

    rare ting

--- test_main.py
+++ test_main.py
@@ -12,5 +12,25 @@ from flask_cors import CORS
 import os
 import requests
 
+
+import pytest
+from flask import Flask
+from main import app
+
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    client = app.test_client()
+    yield client
+
+
+def test_index(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Hello, World!" in response.data
+
+
+
 def test_addition():
     assert 2 + 2 == 4
